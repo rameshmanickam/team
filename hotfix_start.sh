b17119ce391ce1507f0e1866c795422fad93b53d
rameshmanickam <ramesh.manickam@BDC7-L-5464X33.dir.svc.accenture.com>
2016-12-07 14:49:11 +0530

    tst

--- hotfix_start.sh
+++ hotfix_start.sh
@@ -27,7 +27,7 @@ if [ $? -eq 0 ] ; then
 	#git tag -a "${BRANCH_TYPE}/${BRANCH_NAME}" -m "Tag Created for ${BRANCH_TYPE}/${BRANCH_NAME}"
 	git add .
 	git commit -m "CREATING $BRANCH_TYPE: ${BRANCH_NAME} - WITH CONFIGURATION" || echo 'Commit failed. There is probably nothing to commit.'
-	git push
+	git --set-upstream origin $BRANCH_NAME
 	#git push -u "${REPO}" "${BRANCH_TYPE}/${BRANCH_NAME}"
 	cd $WORKSPACE
 	echo "done"
